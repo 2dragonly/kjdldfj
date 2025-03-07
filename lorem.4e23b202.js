c71179916f9611f3d28243be5fd9aa10107c0233
2dragonly <121108445+2dragonly@users.noreply.github.com>
2025-03-07 19:13:07 +0000

    Deploying to gh-pages from @ 2dragonly/kjdldfj@8d9d48867e83bf10278a13751b6d3e87098d0df0 🚀

---
+++ lorem.4e23b202.js
@@ -0,0 +1,23 @@
+
+function $parcel$export(e, n, v, s) {
+  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
+}
+
+      var $parcel$global = globalThis;
+    var parcelRequire = $parcel$global["parcelRequire94c2"];
+var parcelRegister = parcelRequire.register;
+parcelRegister("9jZP8", function(module, exports) {
+
+$parcel$export(module.exports, "Component", () => $42c99d1ef274876b$export$16fa2f45be04daa8);
+
+var $5NSHW = parcelRequire("5NSHW");
+const $42c99d1ef274876b$export$16fa2f45be04daa8 = ()=>{
+    return /*#__PURE__*/ (0, $5NSHW.jsx)("p", {
+        children: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel quam cursus, commodo lectus id, tincidunt magna. Donec aliquet enim urna, in fringilla lacus consectetur eu. Aliquam erat volutpat. Vivamus feugiat consequat lorem a varius. Cras sed magna ac nisi rutrum elementum. In sit amet ultricies metus. Cras non magna augue. Nullam mattis imperdiet eros, et pulvinar erat lacinia ut. Donec dictum lacus maximus elementum ullamcorper. Sed faucibus aliquam dui elementum rhoncus."
+    });
+};
+
+});
+
+
+//# sourceMappingURL=lorem.4e23b202.js.map
